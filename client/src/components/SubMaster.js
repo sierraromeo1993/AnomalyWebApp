e506f85f554dfed52ee3111e76ba41ecd30b6f9e
sierraromeo1993 <61630189+sierraromeo1993@users.noreply.github.com>
2021-05-24 19:14:27 +0300

    change11

--- client/src/components/SubMaster.js
+++ client/src/components/SubMaster.js
@@ -4,6 +4,7 @@ import TestDisplay from './TestDisplay'
 import {useState, useEffect} from 'react'
 import Button from './Button'
 import AddModel from './AddModel';
+import Models from './Models'
 
 
 
@@ -50,7 +51,8 @@ function SubMaster({models,addModel,addFile,testModel, activateAnomalies }) {
 
           
     return (
-        <div>
+        <div className="SubMaster">
+          <Models models={models}/>
           {showAddModel &&<AddModel addModel = {addModel} showAD = {showAD}/>}
           {showButtons &&<Button text={'Train'} type={"Train"} setActiveButton={setActiveButton} activeButton={activeButton}/>}
           {showButtons &&<Button text={'Test'} type={"Test"} setActiveButton={setActiveButton} activeButton={activeButton}/>}
