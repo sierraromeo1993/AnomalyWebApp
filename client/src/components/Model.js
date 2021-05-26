56e53d80919b44d0ba99e7466fe31d354a07a0a2
sierraromeo1993 <61630189+sierraromeo1993@users.noreply.github.com>
2021-05-26 19:23:10 +0300

    small change

--- client/src/components/Model.js
+++ client/src/components/Model.js
@@ -5,9 +5,9 @@ import React from 'react';
 const Model = ({model}) => {
     return (
         <div  className ="Model">
-            <h4>{"Name: "}{model.Name}</h4>
-            <p>{"Type: "}{model.type}</p>
-            <p>{"Ready To Use: "}{model.is_Ready.toString()}</p>
+            <h4>{`Name:  ${model.Name}`}</h4>
+            <p>{`Type:  ${model.type} `}</p>
+            <p>{`Ready To Use: ${(model.is_Ready.toString())}`}</p>
         </div>
     );
 };
